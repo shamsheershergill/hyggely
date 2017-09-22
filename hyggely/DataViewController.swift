a9c9debd59ec8ba177ab5a40f213468f1e796319
Shamsheer <Shamsheer@Shamsheers-MacBook-Air.local>
2017-09-22 16:40:51 -0700

    Initial Commit

---
+++ hyggely/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  hyggely
+//
+//  Created by Shamsheer on 2017-09-22.
+//  Copyright © 2017 Shamsheer. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
