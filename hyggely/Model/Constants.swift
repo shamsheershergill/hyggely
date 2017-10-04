ef7495d207f999d6957d217d9133043055c4656d
Shamsheer <Shamsheer@Shamsheers-MacBook-Air.local>
2017-10-03 19:20:11 -0700

    chat is working

---
+++ hyggely/Model/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  hyggely
+//
+//  Created by Shamsheer on 2017-10-02.
+//  Copyright © 2017 Shamsheer. All rights reserved.
+//
+
+
+import Foundation
+
+class Constants {
+    
+    // DBProvider
+   
+    static let CONTACTS = "Contacts";
+    static let MESSAGES = "Messages";
+ 
+    
+    static let EMAIL = "email";
+    static let PASSWORD = "password";
+    static let DATA = "data";
+    
+    // messages
+    static let TEXT = "text";
+    static let SENDER_ID = "sender_id";
+    static let SENDER_NAME = "sender_name";
+    static let URL = "url";
+    
+}
